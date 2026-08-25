fab5e59a690ccf0087743607c942f4c7672795b8
Tom-J-79 <tom@orbittyres.com>
2026-08-25 16:21:38 +0100

    Restore app.js and load GIFs from repo root

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-PLACEHOLDER
+(() => {
+  const STORAGE = "bellwork.v1";
+  const EX = window.BELLWORK_EXERCISES;
+
+  function art(id) {
+    if (!id) return "";
+    return `<img class=\"ex-svg\" alt=\"\" src=\"art/${id}.gif\" onerror=\"this.onerror=null;this.src='${id}.gif'\">`;
+  }
+})();
